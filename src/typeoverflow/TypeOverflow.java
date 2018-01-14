da672be282a41ec4052f745e0d143f364351d40b
$.m.lnk <smjvlab@gmail.com>
2018-01-15 00:02:22 +0200

    type overflow

---
+++ src/typeoverflow/TypeOverflow.java
@@ -0,0 +1,26 @@
+/* type overflow */
+package typeoverflow;
+
+public class TypeOverflow {
+
+    public static void main(String[] args) {
+        byte a = (byte) (127 + 1);
+        System.out.println(a); // -128 !!! перепонение
+
+        /*(мин.знач. short (16 bit/2 byte,
+          -32768...32767 )) !!! перепонение */
+        short b = (short) (32767 + 1);
+        System.out.println(b); // -32768
+
+        short x = Byte.MAX_VALUE + 1;
+        System.out.println(x); // все ок /*127+1=128*/
+
+        byte y = (byte) x;
+        System.out.println(y);  // -128
+
+        byte z = (byte) (129 + x);
+        System.out.println(z); // 1
+
+    }
+
+}
